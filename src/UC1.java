73bbf5ed6ba803fb7cad74513f2d26765376741b
abhi2005-bit <sinhaabhijit414@gmail.com>
2026-03-29 23:35:03 +0530

    book my stay first

---
+++ src/UC1.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay Application
+ * Version 1.0
+ *
+ * This class represents the entry point of the Hotel Booking System.
+ * It prints a welcome message when the application starts.
+ *
+ * @author Abhijit
+ * @version 1.0
+ */
+public class UC1{
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("==================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Version: 1.0                  ");
+        System.out.println("==================================");
+
+        // End message
+        System.out.println("Application started successfully!");
+    }
+}
